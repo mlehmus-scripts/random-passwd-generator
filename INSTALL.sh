fb779fdc300cd350bc9b14b006738ffba112689a
mlehmus-scripts <mlehmus@gmail.com>
2025-11-29 22:57:19 +0200

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,7 @@
+#This will give permission to executing all .sh -files
+chmod +x *.sh
+echo
+echo Executing permission added.
+echo
+echo How to use password generator tool:
+echo Run generate-pass.sh or open generate-pass folder in terminal and type: ./generate-pass.sh
